0a90da557c6a3a52a8c8b18bb72574d8ba17ba66
Orz1110 <47814958+Orz1110@users.noreply.github.com>
2020-05-15 21:26:15 +0800

    Delete 清洗規則整理.py

--- 中研院/清洗規則整理.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# data = open("C:/Users/Orz/Desktop/1.json")
-#
-# json = json.dumps(data)
-with open("C:/Users/Orz/Desktop/1.json","r",encoding="utf-8") as f:
-	data = json.load(f)
-print(data)
